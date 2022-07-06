0f1cb14ad2d22816cdedfe98c42eb1a0b2eb4f60
comeback-khv <zhigg@mail.ru>
2022-07-06 22:25:06 +1000

    feat: add burger menu and more stuff for mobile version

--- travel/index.js
+++ travel/index.js
@@ -1,1 +1,31 @@
-console.log('Вёрстка валидная + 10\nВёрстка семантическая + 20\nВёрстка соответствует макету +48\nТребования к css + 12\nИнтерактивность, реализуемая через css +20')
+(function () {
+    const burgerItem = document.querySelector('.burger-menu');
+    const nav = document.querySelector('.nav__list');
+    const burgerClose = document.querySelector('.burger-close');
+    const navLinks = document.querySelectorAll('.nav__link');
+    const body = document.querySelector('.body');
+
+    burgerItem.addEventListener('click', () => {
+        burgerClose.classList.add('burger-close--active');
+        nav.classList.add('nav__list--active');
+        body.classList.add('body--active');
+    });
+    burgerClose.addEventListener('click', () => {
+        burgerClose.classList.remove('burger-close--active');
+        nav.classList.remove('nav__list--active');
+        body.classList.remove('body--active');
+    });
+
+    for (let i = 0; i < navLinks.length; i += 1) {
+        navLinks[i].addEventListener('click', () => {
+            burgerClose.classList.remove('burger-close--active');
+            nav.classList.remove('nav__list--active');
+            body.classList.remove('body--active');
+        });
+    }
+
+
+
+}
+
+    ());
