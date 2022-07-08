54974a6d9abd6903f00976590839af6fa0c33662
comeback-khv <zhigg@mail.ru>
2022-07-08 22:59:48 +1000

    feat: finish adaptive for mobile

--- travel/index.js
+++ travel/index.js
@@ -4,6 +4,7 @@
     const burgerClose = document.querySelector('.burger-close');
     const navLinks = document.querySelectorAll('.nav__link');
     const body = document.querySelector('.body');
+    const bodyActive = document.querySelector('.body--active');
 
     burgerItem.addEventListener('click', () => {
         burgerClose.classList.add('burger-close--active');
@@ -16,6 +17,15 @@
         body.classList.remove('body--active');
     });
 
+    document.addEventListener('click', (event => {
+        if (event.target.classList.contains('body--active')) {
+            nav.classList.remove('nav__list--active');
+            body.classList.remove('body--active');
+            burgerClose.classList.remove('burger-close--active');
+        }
+        else return
+    }))
+
     for (let i = 0; i < navLinks.length; i += 1) {
         navLinks[i].addEventListener('click', () => {
             burgerClose.classList.remove('burger-close--active');
@@ -23,9 +33,5 @@
             body.classList.remove('body--active');
         });
     }
-
-
-
 }
-
     ());
